80fa0c1a3d6b7edfc85a56568389f67ecc79fadb
Nicolai Krogh Jørgensen <xy03ze@its.aau.dk>
2023-12-04 16:07:14 +0100

    indent

--- src/main/kotlin/aoc04.kt
+++ src/main/kotlin/aoc04.kt
@@ -7,53 +7,55 @@ fun main(args: Array<String>) {
 }
 
 
-fun getPart41() : Int {
+fun getPart41(): Int {
     val input = File("input04.txt").readLines()
-    val sum  = input.sumOf { card ->
-        card.split(":").last().split("|").map { x -> x.trim().replace("  ", " ").split(" ") }.zipWithNext().map { (winning, scratch) ->
-            val count = winning.intersect(scratch.toSet()).count()
+    val sum = input.sumOf { card ->
+        card.split(":").last().split("|").map { x -> x.trim().replace("  ", " ").split(" ") }.zipWithNext()
+            .map { (winning, scratch) ->
+                val count = winning.intersect(scratch.toSet()).count()
 
-            if (count > 0)
-                multiply(count, 1)
-            else
-                0
-        }.first()
+                if (count > 0)
+                    multiply(count, 1)
+                else
+                    0
+            }.first()
     }
 
     return sum
 }
 
-fun multiply(matchCount : Int, number : Int) : Int {
-    if(matchCount == 1) return number
+fun multiply(matchCount: Int, number: Int): Int {
+    if (matchCount == 1) return number
 
-    return multiply(matchCount-1, number * 2)
+    return multiply(matchCount - 1, number * 2)
 }
 
-fun calcCard(index : Int, scoreMap: MutableMap<Int, Int>, cardValues : List<Int> ) {
-    if(index == cardValues.count()) return
+fun calcCard(index: Int, scoreMap: MutableMap<Int, Int>, cardValues: List<Int>) {
+    if (index == cardValues.count()) return
 
-    for(i in 1..cardValues[index])
-    {
-        scoreMap[index+i] = scoreMap[index+i]!! + scoreMap[index]!!
+    for (i in 1..cardValues[index]) {
+        scoreMap[index + i] = scoreMap[index + i]!! + scoreMap[index]!!
     }
-    calcCard(index+1, scoreMap, cardValues)
+    calcCard(index + 1, scoreMap, cardValues)
 }
+
 fun getPart42(): Int {
     val input = File("input04.txt").readLines()
     val scoreMap =
         (0..input.count())
-        .zip(List(input.count()){ 1 })
-        .toMap().toMutableMap()
+            .zip(List(input.count()) { 1 })
+            .toMap().toMutableMap()
 
-    val cardValues = input.map{ card ->
+    val cardValues = input.map { card ->
         card.split(":").last()
             .split("|")
-            .map { x -> x.trim().replace("  ", " ").split(" ")
-                .map { y -> y.toInt() } }
-            .zipWithNext()
-            .map { (winning, scratch) ->
+            .map { x ->
+                x.trim().replace("  ", " ").split(" ")
+                    .map { y -> y.toInt() }
+            }.zipWithNext()
+            .sumOf { (winning, scratch) ->
                 winning.intersect(scratch.toSet()).count()
-        }.first()
+            }
     }
 
     calcCard(0, scoreMap, cardValues)
