b0d2de2f12a801b353bbbe5081dbf2a6fb95f1b5
Nicolai Krogh Jørgensen <xy03ze@its.aau.dk>
2023-12-08 13:27:22 +0100

    Day 8 2

--- src/main/kotlin/aoc08.kt
+++ src/main/kotlin/aoc08.kt
@@ -1,10 +1,12 @@
 import java.io.File
-import java.util.*
+import java.util.concurrent.atomic.AtomicInteger
+import kotlin.system.measureTimeMillis
 
 fun main(args: Array<String>) {
     println(getPart81())
-    //println(getPart82())
+    println(getPart82())
 }
+
 fun getPart81(): Int {
 
     val input = File("input08.txt").readText()
@@ -14,7 +16,7 @@ fun getPart81(): Int {
 
     var sum = 0
     val instCount = instructions.count()
-    while(currentLocation != "ZZZ"){
+    while(currentLocation != "ZZZ" ){
         val network = networks[currentLocation]!!
         currentLocation = if(instructions[sum % instCount] == 'L') network.first else network.second
         sum++
@@ -22,6 +24,46 @@ fun getPart81(): Int {
     return sum
 }
 
-fun getPart82(): Int {
-    return 0
+fun getPart82(): Long {
+    val input = File("input08.txt").readText()
+    val instructions = input.substringBefore("\r").toList()
+    val networks = Regex("\\w+(?=.*\\))").findAll(input).map { it.value }.chunked(3).associate { Pair(it[0], Pair(it[1], it[2])) }
+    val startNetworks = networks.filterKeys { it.endsWith("A") }.map { it.key }
+
+    val indexes = mutableListOf<Int>()
+
+    startNetworks.parallelStream().forEach { startPosition ->
+        var currentLocation = startPosition
+        var index = 0
+        val instCount = instructions.count()
+
+        while(!currentLocation.endsWith("Z")){
+            val network = networks[currentLocation]!!
+            currentLocation = if(instructions[index++ % instCount] == 'L') network.first else network.second
+        }
+        indexes.add(index)
+    }
+
+    return findLCMOfListOfNumbers(indexes.map { it.toLong() })
+}
+
+fun findLCM(a: Long, b: Long): Long {
+    val larger = if (a > b) a else b
+    val maxLcm = a * b
+    var lcm = larger
+    while (lcm <= maxLcm) {
+        if (lcm % a == 0L && lcm % b == 0L) {
+            return lcm
+        }
+        lcm += larger
+    }
+    return maxLcm
+}
+
+fun findLCMOfListOfNumbers(numbers: List<Long>): Long {
+    var result = numbers[0]
+    for (i in 1..<numbers.size) {
+        result = findLCM(result, numbers[i])
+    }
+    return result
 }
